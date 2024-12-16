6d0f2954b0454535f4cfe6dcc23d670ebc236d4d
Huy Nguyen <i.huy@outlook.com>
2024-12-15 23:57:41 -0800

    Added CoreUI Login page and assets

--- app.py
+++ app.py
@@ -7,18 +7,34 @@ import json
 app = Flask(__name__)
 app.config.from_object("config.Config")
 
-app.config['UPLOAD_FOLDER']  = "./static/uploads/"
 
-# @app.route('/static/<path:filename>')
-# def serve_static_assets_folder(filename):
-#     return send_from_directory('./static/assets', filename)
 
+
+# Import Routes 
 from routes.category import init_category_routes
 init_category_routes(app)
 from routes.upload import init_upload_routes
 init_upload_routes(app)
 from routes.dashboard import init_dashboard_routes
 init_dashboard_routes(app)
+
+app.config['UPLOAD_FOLDER']  = "./static/uploads/"
+# Static folders
+@app.route('/vendors/<path:filename>')
+def serve_static_vendors_folder(filename):
+    return send_from_directory('./static/vendors/', filename)
+
+@app.route('/js/<path:filename>')
+def serve_static_js_folder(filename):
+    return send_from_directory('./static/js/', filename)
+
+@app.route('/css/<path:filename>')
+def serve_static_css_folder(filename):
+    return send_from_directory('./static/css/', filename)
+
+@app.route('/assets/<path:filename>')
+def serve_static_assets_folder(filename):
+    return send_from_directory('./static/assets/', filename)
 
 # Routes
 @app.route('/', methods=['GET', 'POST'])
