d0fb6d3a8fa050188af7426b110b0cdba90f0505
Huy Nguyen <i.huy@outlook.com>
2024-12-17 01:15:25 -0800

    Update requirements & Get rid of coreui

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-import os
-from hashlib import md5
 from flask import Flask, render_template, request, redirect, url_for, session, send_from_directory
-from models.db import connection
-import json
 
 app = Flask(__name__)
 app.config.from_object("config.Config")
 
-
+# Static folders
+app.config['UPLOAD_FOLDER']  = "./static/uploads/"
 
 
 # Import Routes 
@@ -17,24 +14,6 @@ from routes.upload import init_upload_routes
 init_upload_routes(app)
 from routes.dashboard import init_dashboard_routes
 init_dashboard_routes(app)
-
-app.config['UPLOAD_FOLDER']  = "./static/uploads/"
-# Static folders
-@app.route('/vendors/<path:filename>')
-def serve_static_vendors_folder(filename):
-    return send_from_directory('./static/vendors/', filename)
-
-@app.route('/js/<path:filename>')
-def serve_static_js_folder(filename):
-    return send_from_directory('./static/js/', filename)
-
-@app.route('/css/<path:filename>')
-def serve_static_css_folder(filename):
-    return send_from_directory('./static/css/', filename)
-
-@app.route('/assets/<path:filename>')
-def serve_static_assets_folder(filename):
-    return send_from_directory('./static/assets/', filename)
 
 # Routes
 @app.route('/', methods=['GET', 'POST'])
