a85319ebcb2fc65f4c005630278f24ffbbb782eb
Huy Nguyen <i.huy@outlook.com>
2024-12-17 23:12:26 -0800

    fix home url

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session, send_from_directory
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
 app.config.from_object("config.Config")
