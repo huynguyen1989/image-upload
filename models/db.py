b0b76f2b9e26fb961ca7e31417e0e32c12ea495a
Huy Nguyen <i.huy@outlook.com>
2024-12-18 20:19:19 -0800

    Update DB Connection auto-reconnect

--- models/db.py
+++ models/db.py
@@ -2,25 +2,35 @@ import pymysql.cursors
 import pymysql
 import functools
 
+host='localhost'
+user='admin'
+password='12345678'
+database='SecondTask'
+
 def auto_reconnect(func):
     @functools.wraps(func)
     def wrapper( *args, **kwargs):
         weakConnection = None
         try:
+            print("Attempting connect to DB")
             weakConnection = func( *args, **kwargs)
             return weakConnection
         except pymysql.err.OperationalError as e:
             print(f"DB Connection error: {e}")
-            weakConnection.reconnect()
-            return func( *args, **kwargs)
+            weakConnection.connect(host=host,
+                             user=user,
+                             password=password,
+                             database=database,
+                             cursorclass=pymysql.cursors.DictCursor)
+            return weakConnection
     return wrapper
 
 @auto_reconnect
 def attemptConnectToDB():
-    return pymysql.connect(host='localhost',
-                             user='admin',
-                             password='12345678',
-                             database='SecondTask',
+    return pymysql.connect(host=host,
+                             user=user,
+                             password=password,
+                             database=database,
                              cursorclass=pymysql.cursors.DictCursor)
     
 
